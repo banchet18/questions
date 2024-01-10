145350ab45b0cd636e807970648ae46c4713e229
banchet18 <banjarac6@gmail.com>
2024-01-10 12:11:11 +0530

    c

---
+++ var,let,const/script.js
@@ -0,0 +1,6 @@
+// var  ,let , const
+
+// What is scope
+// scope is a certain region of program where a defined variable exist and can be recognized and beyond that it cannot be recognized.
+// so there can be multiple types of scope
+//eg global scope, block scope, functional scope
