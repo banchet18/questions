595789c43a8a6222530ecbd83a65bd19d9186594
banchet18 <banjarac6@gmail.com>
2024-01-11 08:18:53 +0530

    c

---
+++ 3.functions/script.js
@@ -0,0 +1,9 @@
+// functions
+//Q1 what is function declaration
+function hello(num) {
+  return num * num;
+}
+//Q2 what is function expression
+const square = function (num) {
+  return num * num;
+};
